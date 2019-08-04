cb2fc5388bdacc4a5c1e9cb3431611ea53e9da04
Da Zheng <zhengda1936@gmail.com>
2019-08-03 17:02:53 -0700

    update movielens.

--- 3_recommender_system/movielens.py
+++ 3_recommender_system/movielens.py
@@ -3,12 +3,41 @@ import pandas as pd
 import numpy as np
 import re
 import stanfordnlp
-import torch
-import dgl
+import string
 import tqdm
+from functools import partial
 
 class MovieLens(object):
     split_by_time = None
+    
+    def read_user_line(self, l):
+        id_, age, gender, occupation, zip_ = l.strip().split('|')
+        age = np.searchsorted([20, 30, 40, 50, 60], age)   # bin the ages into <20, 20-30, 30-40, ..., >60
+        return {'id': int(id_), 'gender': gender, 'age': age, 'occupation': occupation, 'zip': zip_}
+    
+    def read_product_line(self, l):
+        fields = l.strip().split('|')
+        id_ = fields[0]
+        title = fields[1]
+        genres = fields[-19:]
+
+        # extract year
+        if re.match(r'.*\([0-9]{4}\)$', title):
+            year = title[-5:-1]
+            title = title[:-6].strip()
+        else:
+            year = 0
+
+        data = {'id': int(id_), 'title': title, 'year': year}
+        for i, g in enumerate(genres):
+            g = int(g)
+            data['genre' + str(i)] = (g != 0)
+        return data
+    
+    def read_rating_line(self, l):
+        user_id, product_id, rating, timestamp = l.split()
+        return {'user_id': int(user_id), 'product_id': int(product_id), 'rating': float(rating), 'timestamp': int(timestamp)}
+    
     def __init__(self, directory):
         '''
         directory: path to movielens directory which should have the three
@@ -24,19 +53,15 @@ class MovieLens(object):
         ratings = []
 
         # read ratings
-        with open(os.path.join(directory, 'ratings.dat')) as f:
+        with open(os.path.join(directory, 'ua.base')) as f:
             for l in f:
-                user_id, product_id, rating, timestamp = l.split('::')
-                user_id = int(user_id)
-                product_id = int(product_id)
-                rating = float(rating)
-                timestamp = int(timestamp)
-                ratings.append({
-                    'user_id': user_id,
-                    'product_id': product_id,
-                    'rating': rating,
-                    'timestamp': timestamp,
-                    })
+                rating = self.read_rating_line(l)
+                ratings.append(rating)
+        with open(os.path.join(directory, 'ua.test')) as f:
+            for l in f:
+                rating = self.read_rating_line(l)
+                ratings.append(rating)
+                
         ratings = pd.DataFrame(ratings)
         product_count = ratings['product_id'].value_counts()
         product_count.name = 'product_count'
@@ -45,42 +70,19 @@ class MovieLens(object):
 
         # read users - if user feature does not exist, we find all unique user IDs
         # appeared in the rating table and create an empty table from that.
-        user_file = os.path.join(directory, 'users.dat')
-        if os.path.exists(user_file):
-            with open(user_file) as f:
-                for l in f:
-                    id_, gender, age, occupation, zip_ = l.strip().split('::')
-                    users.append({
-                        'id': int(id_),
-                        'gender': gender,
-                        'age': age,
-                        'occupation': occupation,
-                        'zip': zip_,
-                        })
-            self.users = pd.DataFrame(users).set_index('id').astype('category')
-        else:
-            users = [{'id': id_} for id_ in ratings['user_id'].unique()]
-            self.users = pd.DataFrame(users).set_index('id')
+        user_file = os.path.join(directory, 'u.user')
+        with open(user_file) as f:
+            for l in f:
+                users.append(self.read_user_line(l))
+        self.users = pd.DataFrame(users).set_index('id').astype('category')
 
         # read products
-        with open(os.path.join(directory, 'movies.dat'), encoding='latin1') as f:
+        with open(os.path.join(directory, 'u.item'), encoding='latin1') as f:
             for l in f:
-                id_, title, genres = l.strip().split('::')
-                genres_set = set(genres.split('|'))
-
-                # extract year
-                assert re.match(r'.*\([0-9]{4}\)$', title)
-                year = title[-5:-1]
-                title = title[:-6].strip()
-
-                data = {'id': int(id_), 'title': title, 'year': year}
-                for g in genres_set:
-                    data[g] = True
-                products.append(data)
+                products.append(self.read_product_line(l))
         self.products = (
                 pd.DataFrame(products)
                 .set_index('id')
-                .fillna(False)
                 .astype({'year': 'category'}))
         self.genres = self.products.columns[self.products.dtypes == bool]
         
@@ -126,6 +128,11 @@ class MovieLens(object):
 
     # process the features and build the DGL graph
     def build_graph(self):
+        import mxnet as mx
+        from mxnet import ndarray as nd
+        from mxnet import gluon, autograd
+        import dgl
+        
         user_ids = list(self.users.index)
         product_ids = list(self.products.index)
         user_ids_invmap = {id_: i for i, id_ in enumerate(user_ids)}
@@ -138,27 +145,35 @@ class MovieLens(object):
         g = dgl.DGLGraph(multigraph=True)
         g.add_nodes(len(user_ids) + len(product_ids))
 
+        # node type
+        node_type = nd.zeros(g.number_of_nodes(), dtype='float32')
+        node_type[:len(user_ids)] = 1
+        g.ndata['type'] = node_type
+
         # user features
+        print('Adding user features...')
         for user_column in self.users.columns:
-            udata = torch.zeros(g.number_of_nodes(), dtype=torch.int64)
+            udata = nd.zeros(g.number_of_nodes(), dtype='int64')
             # 0 for padding
             udata[:len(user_ids)] = \
-                    torch.LongTensor(self.users[user_column].cat.codes.values.astype('int64') + 1)
+                    nd.from_numpy(self.users[user_column].cat.codes.values.astype('int64') + 1)
             g.ndata[user_column] = udata
 
         # product genre
-        product_genres = torch.from_numpy(self.products[self.genres].values.astype('float32'))
-        g.ndata['genre'] = torch.zeros(g.number_of_nodes(), len(self.genres))
+        print('Adding product features...')
+        product_genres = nd.from_numpy(self.products[self.genres].values.copy().astype('float32'))
+        g.ndata['genre'] = nd.zeros((g.number_of_nodes(), len(self.genres)))
         g.ndata['genre'][len(user_ids):len(user_ids) + len(product_ids)] = product_genres
 
         # product year
         if 'year' in self.products.columns:
-            g.ndata['year'] = torch.zeros(g.number_of_nodes(), dtype=torch.int64)
+            g.ndata['year'] = nd.zeros(g.number_of_nodes(), dtype='int64')
             # 0 for padding
             g.ndata['year'][len(user_ids):len(user_ids) + len(product_ids)] = \
-                    torch.LongTensor(self.products['year'].cat.codes.values.astype('int64') + 1)
+                    nd.from_numpy(self.products['year'].cat.codes.values.astype('int64') + 1)
 
         # product title
+        print('Parsing title...')
         nlp = stanfordnlp.Pipeline(use_gpu=False, processors='tokenize,lemma')
         vocab = set()
         title_words = []
@@ -173,12 +188,12 @@ class MovieLens(object):
         vocab = list(vocab)
         vocab_invmap = {w: i for i, w in enumerate(vocab)}
         # bag-of-words
-        g.ndata['title'] = torch.zeros(g.number_of_nodes(), len(vocab))
+        g.ndata['title'] = nd.zeros((g.number_of_nodes(), len(vocab)))
         for i, tw in enumerate(tqdm.tqdm(title_words)):
-            g.ndata['title'][i, [vocab_invmap[w] for w in tw]] = 1
+            g.ndata['title'][len(user_ids) + i, [vocab_invmap[w] for w in tw]] = 1
         self.vocab = vocab
         self.vocab_invmap = vocab_invmap
-
+        
         rating_user_vertices = [user_ids_invmap[id_] for id_ in self.ratings['user_id'].values]
         rating_product_vertices = [product_ids_invmap[id_] + len(user_ids)
                                  for id_ in self.ratings['product_id'].values]
@@ -188,21 +203,23 @@ class MovieLens(object):
         g.add_edges(
                 rating_user_vertices,
                 rating_product_vertices,
-                data={'inv': torch.zeros(self.ratings.shape[0], dtype=torch.uint8),
-                    'rating': torch.FloatTensor(self.ratings['rating'])})
+                data={'inv': nd.zeros(self.ratings.shape[0], dtype='int32'),
+                    'rating': nd.from_numpy(self.ratings['rating'].values.astype('float32'))})
         g.add_edges(
                 rating_product_vertices,
                 rating_user_vertices,
-                data={'inv': torch.ones(self.ratings.shape[0], dtype=torch.uint8),
-                    'rating': torch.FloatTensor(self.ratings['rating'])})
+                data={'inv': nd.ones(self.ratings.shape[0], dtype='int32'),
+                    'rating': nd.from_numpy(self.ratings['rating'].values.astype('float32'))})
         self.g = g
+        g.readonly()
         
     # Assign masks of training, validation and test set onto the DGL graph
     # according to the rating table.
     def generate_mask(self):
-        valid_tensor = torch.from_numpy(self.ratings['valid'].values.astype('uint8'))
-        test_tensor = torch.from_numpy(self.ratings['test'].values.astype('uint8'))
-        train_tensor = torch.from_numpy(self.ratings['train'].values.astype('uint8'))
+        from mxnet import ndarray as nd
+        valid_tensor = nd.from_numpy(self.ratings['valid'].values.astype('int32'))
+        test_tensor = nd.from_numpy(self.ratings['test'].values.astype('int32'))
+        train_tensor = nd.from_numpy(self.ratings['train'].values.astype('int32'))
         edge_data = {
                 'valid': valid_tensor,
                 'test': test_tensor,
